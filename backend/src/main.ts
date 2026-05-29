012529efe7b5af850d46dc85e6761aeb21e300ed
Althaf2102 <fakhriza_faraby_33rpl@student.smktelkom-mlg.sch.id>
2026-05-29 21:39:05 +0700

    sync

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -27,6 +27,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  // Menggunakan port dinamis dari Railway, atau port 3000 jika di laptop sendiri
+const port = process.env.PORT || 3000;
+await app.listen(port);
 }
 bootstrap();
