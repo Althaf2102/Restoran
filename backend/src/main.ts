bceb968c95f254c0cbc67b232021eeb835349101
Althaf2102 <fakhriza_faraby_33rpl@student.smktelkom-mlg.sch.id>
2026-05-29 18:21:56 +0700

    add CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+    app.enableCors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
