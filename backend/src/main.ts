a3b0138113789cbf447704bd77bd1d440d2c4e13
Althaf2102 <fakhriza_faraby_33rpl@student.smktelkom-mlg.sch.id>
2026-06-01 19:05:48 +0700

    fix CORS options

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,11 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+ app.enableCors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false,
+});
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
